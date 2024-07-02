7e9102f1fe383a70fe505672a18d47a946005fae
Raymond Liang <raymondliang2015@gmail.com>
2024-07-02 16:54:39 -0700

    add authentication with clerk

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <p>Home</p>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+
+            <UserButton />
+        </div>
     )
 }
 
